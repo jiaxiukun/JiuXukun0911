1462e427a7592cef96d3a3a1c47c41784ca48b63
jiaxiukun <453766331@qq.com>
2017-09-11 10:23:15 +0800

    Initial commit

---
+++ app/src/main/java/baway/com/jiuxukun0911/Bean/CartBean.java
@@ -0,0 +1,16 @@
+package baway.com.jiuxukun0911.Bean;
+
+/**
+ * Created by Administrator on 2017/9/11.
+ * 店铺和商品IDBean
+ * 王野
+ */
+
+public class CartBean {
+
+    /** 商品ID */
+    private String goodsid;
+    /** 店铺ID */
+    private String storeid;
+
+}
